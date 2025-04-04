63d00f17354ce816b7923c8f306d233690440f94
Brian Guzmán <b.guzman.m@me.com>
2025-04-04 19:22:25 -0300

    nueva rama con cambios cosméticos

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -19,17 +19,17 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          la cuenta es de {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edita <code>src/App.jsx</code> y guarda los campos para testear
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click en los logos de Vite y React para aprender mas
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
